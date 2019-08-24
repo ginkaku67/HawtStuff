4078b50ffddc3cb389d182d36077b281942e87f2
Nick Gluch <nickrgluch@yahoo.com>
2019-08-24 13:11:47 -0500

    fix submit

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ var tableReservations = [
   }
 ];
 //routes
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 // Displays a single tableReservation, or returns false
-app.get("/tables/:tableReservations", function(req, res) {
+app.get("/tables/:tableReservations", function (req, res) {
   var tabeel = req.params.tableReservations;
   console.log(tabeel);
   for (var i = 0; i < tableReservations.length; i++) {
@@ -39,19 +39,36 @@ app.get("/tables/:tableReservations", function(req, res) {
   }
   return res.json(false);
 });
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-  console.log(newCharacter);
-  characters.push(newCharacter);
-  res.json(newCharacter);
-});
+
+if (tableReservations.length < 5) {
+
+  app.post("/api/currentReservation", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.id = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newReservation);
+
+    tableReservations.push(newReservation);
+
+    res.json(newReservation);
+    console.log('this if works')
+  });
+}
+else {
+  console.log('this else works')
+}
+
+
+
+
+
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
